96078c177b68dad3bd07903392b6dd4e0002ba41
Thumbsy <thumbseh@gmail.com>
2016-09-23 15:38:12 +0200

    HsH-Vol.2 readmes and ratings scripts

---
+++ High-speed Happiness Vol.2/Convert HsH-Vol.2 to DDRX ratings.bat
@@ -0,0 +1,70 @@
+@ECHO OFF
+
+echo ::::: Applying METERTYPE correction :::::
+fnr.exe --cl --dir "%~dp0\." --includeSubDirectories --fileMask "*.sm" --find "#METERTYPE:ITG;" --replace "#METERTYPE:DDR X;"
+
+echo ::::: Applying chart ratings for float on flowers [Thumbsy] :::::
+fnr.exe --cl --dir "%~dp0\float on flowers [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Beginner:\r\n     3" --replace "Beginner:\r\n     4"
+fnr.exe --cl --dir "%~dp0\float on flowers [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Easy:\r\n     5" --replace "Easy:\r\n     7"
+fnr.exe --cl --dir "%~dp0\float on flowers [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Medium:\r\n     7" --replace "Medium:\r\n     10"
+fnr.exe --cl --dir "%~dp0\float on flowers [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Hard:\r\n     9" --replace "Hard:\r\n     13"
+fnr.exe --cl --dir "%~dp0\float on flowers [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Challenge:\r\n     10" --replace "Challenge:\r\n     15"
+
+echo ::::: Applying chart ratings for Hard Humming [Thumbsy] :::::
+fnr.exe --cl --dir "%~dp0\Hard Humming [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Easy:\r\n     4" --replace "Easy:\r\n     6"
+fnr.exe --cl --dir "%~dp0\Hard Humming [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Medium:\r\n     5" --replace "Medium:\r\n     7"
+fnr.exe --cl --dir "%~dp0\Hard Humming [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Hard:\r\n     7" --replace "Hard:\r\n     10"
+
+echo ::::: Applying chart ratings for I'll stop this [Thumbsy] :::::
+fnr.exe --cl --dir "%~dp0\I'll stop this [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Beginner:\r\n     2" --replace "Beginner:\r\n     3"
+fnr.exe --cl --dir "%~dp0\I'll stop this [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Easy:\r\n     4" --replace "Easy:\r\n     6"
+fnr.exe --cl --dir "%~dp0\I'll stop this [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Medium:\r\n     6" --replace "Medium:\r\n     8"
+fnr.exe --cl --dir "%~dp0\I'll stop this [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Hard:\r\n     8" --replace "Hard:\r\n     11"
+fnr.exe --cl --dir "%~dp0\I'll stop this [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Challenge:\r\n     10" --replace "Challenge:\r\n     14"
+
+echo ::::: Applying chart ratings for Kiriban Getter [Thumbsy] :::::
+fnr.exe --cl --dir "%~dp0\Kiriban Getter [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Easy:\r\n     3" --replace "Easy:\r\n     5"
+fnr.exe --cl --dir "%~dp0\Kiriban Getter [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Medium:\r\n     7" --replace "Medium:\r\n     9"
+fnr.exe --cl --dir "%~dp0\Kiriban Getter [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Hard:\r\n     8" --replace "Hard:\r\n     11"
+fnr.exe --cl --dir "%~dp0\Kiriban Getter [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Challenge:\r\n     11" --replace "Challenge:\r\n     15"
+
+echo ::::: Applying chart ratings for Lift Me Up [Thumbsy] :::::
+fnr.exe --cl --dir "%~dp0\Lift Me Up [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Easy:\r\n     4" --replace "Easy:\r\n     6"
+fnr.exe --cl --dir "%~dp0\Lift Me Up [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Medium:\r\n     6" --replace "Medium:\r\n     8"
+fnr.exe --cl --dir "%~dp0\Lift Me Up [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Hard:\r\n     8" --replace "Hard:\r\n     11"
+fnr.exe --cl --dir "%~dp0\Lift Me Up [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Challenge:\r\n     9" --replace "Challenge:\r\n     12"
+
+echo ::::: Applying chart ratings for LOOKBACK [Thumbsy] :::::
+fnr.exe --cl --dir "%~dp0\LOOKBACK [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Beginner:\r\n     3" --replace "Beginner:\r\n     4"
+fnr.exe --cl --dir "%~dp0\LOOKBACK [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Easy:\r\n     5" --replace "Easy:\r\n     7"
+fnr.exe --cl --dir "%~dp0\LOOKBACK [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Medium:\r\n     8" --replace "Medium:\r\n     10"
+fnr.exe --cl --dir "%~dp0\LOOKBACK [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Hard:\r\n     10" --replace "Hard:\r\n     14"
+fnr.exe --cl --dir "%~dp0\LOOKBACK [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Challenge:\r\n     11" --replace "Challenge:\r\n     16"
+
+echo ::::: Applying chart ratings for My Direction (Squad-E Remix) [Thumbsy] :::::
+fnr.exe --cl --dir "%~dp0\My Direction (Squad-E Remix) [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Beginner:\r\n     3" --replace "Beginner:\r\n     4"
+fnr.exe --cl --dir "%~dp0\My Direction (Squad-E Remix) [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Easy:\r\n     4" --replace "Easy:\r\n     6"
+fnr.exe --cl --dir "%~dp0\My Direction (Squad-E Remix) [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Medium:\r\n     7" --replace "Medium:\r\n     9"
+fnr.exe --cl --dir "%~dp0\My Direction (Squad-E Remix) [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Hard:\r\n     8" --replace "Hard:\r\n     11"
+
+echo ::::: Applying chart ratings for Statice (XIO Remix) [Thumbsy feat. Nezumi] :::::
+fnr.exe --cl --dir "%~dp0\Statice (XIO Remix) [Thumbsy feat. Nezumi]" --fileMask "*.sm" --useEscapeChars --find "Beginner:\r\n     3" --replace "Beginner:\r\n     4"
+fnr.exe --cl --dir "%~dp0\Statice (XIO Remix) [Thumbsy feat. Nezumi]" --fileMask "*.sm" --useEscapeChars --find "Easy:\r\n     5" --replace "Easy:\r\n     7"
+fnr.exe --cl --dir "%~dp0\Statice (XIO Remix) [Thumbsy feat. Nezumi]" --fileMask "*.sm" --useEscapeChars --find "Medium:\r\n     6" --replace "Medium:\r\n     8"
+fnr.exe --cl --dir "%~dp0\Statice (XIO Remix) [Thumbsy feat. Nezumi]" --fileMask "*.sm" --useEscapeChars --find "Hard:\r\n     8" --replace "Hard:\r\n     11"
+
+echo ::::: Applying chart ratings for Top of The World (nanobii Remix) [Thumbsy] :::::
+fnr.exe --cl --dir "%~dp0\Top of The World (nanobii Remix) [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Beginner:\r\n     2" --replace "Beginner:\r\n     3"
+fnr.exe --cl --dir "%~dp0\Top of The World (nanobii Remix) [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Easy:\r\n     3" --replace "Easy:\r\n     5"
+fnr.exe --cl --dir "%~dp0\Top of The World (nanobii Remix) [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Medium:\r\n     5" --replace "Medium:\r\n     8"
+fnr.exe --cl --dir "%~dp0\Top of The World (nanobii Remix) [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Hard:\r\n     7" --replace "Hard:\r\n     11"
+
+echo ::::: Applying chart ratings for You're Everything [Thumbsy] :::::
+fnr.exe --cl --dir "%~dp0\You're Everything [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Beginner:\r\n     2" --replace "Beginner:\r\n     3"
+fnr.exe --cl --dir "%~dp0\You're Everything [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Easy:\r\n     4" --replace "Easy:\r\n     6"
+fnr.exe --cl --dir "%~dp0\You're Everything [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Medium:\r\n     7" --replace "Medium:\r\n     10"
+fnr.exe --cl --dir "%~dp0\You're Everything [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Hard:\r\n     9" --replace "Hard:\r\n     12"
+fnr.exe --cl --dir "%~dp0\You're Everything [Thumbsy]" --fileMask "*.sm" --useEscapeChars --find "Challenge:\r\n     10" --replace "Challenge:\r\n     15"
+
+echo Ratings should now have been changed to DDR X scale.
+pause
---
+++ High-speed Happiness Vol.2/readme.txt
@@ -0,0 +1,39 @@
+Thanks for trying out this little pack! Second in the HsH series, the charts in this one are all mine this time.
+The only file to note here is Statice (XIO Remix), which was originally going to appear in a pack Nez and I were working on (but never really got around to finishing.... yet). Nez made the graphics on that one. Everything else was edited by me.
+
+
+
+===== Graphics credits/sources =====
+Pack banner/jacket illustration by MiMikuChair http://mimikuchair.deviantart.com/
+
+float on flowers [Thumbsy]:
+	花浮かぶ島 http://www.pixiv.net/member_illust.php?mode=medium&illust_id=56330203 // By ゆう http://www.pixiv.net/member.php?id=6669139
+
+Hard Humming [Thumbsy]:
+	I literally googled 'nose' and went to images.
+
+I'll stop this [Thumbsy]:
+	・。☆。・ http://www.pixiv.net/member_illust.php?mode=medium&illust_id=43595396 // By まふゆ http://www.pixiv.net/member.php?id=5229572
+	Above The World cover artwork by Yusie https://twitter.com/Untitled_yusie (the album's free, go get it here: http://pwk001.tumblr.com/)
+
+Kiriban Getter [Thumbsy]:
+	http://1.bp.blogspot.com/-zr_yAZrWVWo/VAK1DOh50yI/AAAAAAAACMM/eX760i-Qzmk/s1600/F5-Button-Webpage-Refresh.jpg
+	http://www.unoosa.org/res/timeline/index_html/space-2.jpg
+
+Lift Me Up [Thumbsy]:
+	Lift Me Up cover artwork (https://soundcloud.com/ultravibes/ultravibes-lift-me-up-preview)
+
+LOOKBACK [Thumbsy]:
+	Unfolding by Musback. http://musback.com/ (http://musback.com/unfolding-wallpaper/)
+
+My Direction (Squad-E Remix) [Thumbsy]:
+	我妻由乃 http://www.pixiv.net/member_illust.php?mode=medium&illust_id=22937528 // By ゆん http://www.pixiv.net/member.php?id=889352
+
+Statice (XIO Remix) [Thumbsy feat. Nezumi]:
+	Reminisphere cover artwork by にもし http://www.pixiv.net/member.php?id=145944 (http://spriterecordings.upper.jp/reminisphere/)
+
+Top of The World (nanobii Remix) [Thumbsy]:
+	Top of The World (nanobii Remix) cover artwork by TheRyDesigns https://twitter.com/TheRyDesign (https://soundcloud.com/nanobii/totw)
+
+You're Everything [Thumbsy]:
+	みくちゃん http://www.pixiv.net/member_illust.php?mode=medium&illust_id=52760642 // By 潰れトマト http://www.pixiv.net/member.php?id=1898417
