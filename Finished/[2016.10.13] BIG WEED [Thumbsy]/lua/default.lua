665e40b7774dc4503ed73c6060b7bd5c0f3fd22c
Thumbsy <thumbseh@gmail.com>
2017-08-23 15:52:46 +0200

    BIG WEED: Updated Lua to detect number of players

--- Finished/[2016.10.13] BIG WEED [Thumbsy]/lua/default.lua
+++ Finished/[2016.10.13] BIG WEED [Thumbsy]/lua/default.lua
@@ -1,7 +1,5 @@
 -- Template taken from Puurokulho's wndrwll: https://www.youtube.com/watch?v=KDWAGbwdA_w
 
-local songName= GAMESTATE:GetCurrentSong():GetSongDir()
-
 local function song_mod_internal(str, pn)
 	local ps= GAMESTATE:GetPlayerState(pn)
 	local pmods= ps:GetPlayerOptionsString('ModsLevel_Song')
@@ -20,11 +18,11 @@ end
 local function song_init()
 	fgcurcommand = 0;
 	checked = false;
-	
+	max_players = #GAMESTATE:GetHumanPlayers()
 	
 	
 	-- lua course :D	/ timed mod management	
-	curmod = 1;
+	curmod = 1
 	-- {beat,mod,player}
 	mods = {
 
@@ -37,7 +35,7 @@ local function song_init()
 	}
 
 	-- timed message broadcaster
-	curmessage = 1;
+	curmessage = 1
 	-- {beat,message,ignoreIfAhead}
 	messages = {
 
@@ -58,30 +56,12 @@ end
 local function song_update()
 
 	if GAMESTATE:GetSongBeat()>=0.1 and not checked then
-	
-		if SCREENMAN:GetTopScreen():GetChild('PlayerP1') then
-			P1 = SCREENMAN:GetTopScreen():GetChild('PlayerP1')
-		end
-		
-		if P1 then
-			P1xpos=P1:GetX();
-			P1ypos=P1:GetY();
-		end
-		
-		if SCREENMAN:GetTopScreen():GetChild('PlayerP2') then
-			P2 = SCREENMAN:GetTopScreen():GetChild('PlayerP2')
-		end
-		
-		if P2 then
-			P2xpos=P2:GetX();
-			P2ypos=P2:GetY();
-		end
 				
-		screen = SCREENMAN:GetTopScreen();
-		screen:stopeffect();
-		screen:x(0);
+		screen = SCREENMAN:GetTopScreen()
+		screen:stopeffect()
+		screen:x(0)
 		
-		checked = true;
+		checked = true
 		
 	end
 	
@@ -112,10 +92,10 @@ local function song_update()
 	
 	while curmessage<= #messages and GAMESTATE:GetSongBeat()>=messages[curmessage][1] do
 		if messages[curmessage][3] and GAMESTATE:GetSongBeat()>=messages[curmessage][1]+5 then
-			curmessage = curmessage+1;
+			curmessage = curmessage+1
 		else
 			MESSAGEMAN:Broadcast(messages[curmessage][2])
-			curmessage = curmessage+1;
+			curmessage = curmessage+1
 		end
 	end
 end
@@ -134,43 +114,43 @@ return Def.ActorFrame{
 		OnCommand= cmd(diffuse,0,0,0,1;diffusealpha,0;zoomto,6,SCREEN_HEIGHT;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y),
 		
 		ScreenRotateZ10MessageCommand= function(self)
-			screen:rotationz(10);
+			screen:rotationz(10)
 		end,
 		
 		ScreenRotateZ0MessageCommand= function(self)
-			screen:rotationz(0);
+			screen:rotationz(0)
 		end,
 		
 		ScreenShiftMessageCommand= function(self)
-			screen:addx(-100);
+			screen:addx(-100)
 		end,
 		
 		ScreenResetShiftMessageCommand= function(self)
-			screen:addx(100);
+			screen:addx(100)
 		end,
 		
 		ScreenBobMessageCommand= function(self)
-			screen:bob();
-			screen:effectmagnitude(40,0,0);
-			screen:effectperiod(1);
-			screen:effectclock('beat');
+			screen:bob()
+			screen:effectmagnitude(40,0,0)
+			screen:effectperiod(1)
+			screen:effectclock('beat')
 		end,
 		
 		ScreenBoounceMessageCommand= function(self)
-			screen:bounce();
-			screen:effectmagnitude(0,-50,0);
-			screen:effectperiod(1);
-			screen:effectclock('beat');
+			screen:bounce()
+			screen:effectmagnitude(0,-50,0)
+			screen:effectperiod(1)
+			screen:effectclock('beat')
 		end,
 		
 		CompleteMessMessageCommand= function(self)
-			SCREENMAN:SystemMessage('S E N D H E L P');
-			screen:vibrate();
-			screen:effectmagnitude(20,20,20);
+			SCREENMAN:SystemMessage('S E N D H E L P')
+			screen:vibrate()
+			screen:effectmagnitude(20,20,20)
 						
-			for i=1,2 do
-				if ('P' .. i) then
-					local pl = screen:GetLifeMeter('PlayerNumber_P' .. i);
+			for i=1,max_players do
+				if SCREENMAN:GetTopScreen():GetChild('PlayerP' .. i) then
+					local pl = screen:GetLifeMeter('PlayerNumber_P' .. i)
 					pl:spin();
 					pl:effectperiod(1);
 					pl:effectclock('beat');
@@ -182,9 +162,9 @@ return Def.ActorFrame{
 		StopEverythingMessageCommand= function(self)
 			screen:stopeffect();
 			
-			for i=1,2 do
-				if ('P' .. i) then
-					screen:GetLifeMeter('PlayerNumber_P' .. i):stopeffect();
+			for i=1,max_players do
+				if SCREENMAN:GetTopScreen():GetChild('PlayerP' .. i) then
+					screen:GetLifeMeter('PlayerNumber_P' .. i):stopeffect()
 				end
 			end
 			
